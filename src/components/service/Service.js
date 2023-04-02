b9b18ba5e2d5e29010fc942ec01cd1ce2f5c9e1f
ulanbekov1 <musabaibolot@gmail.com>
2023-04-02 11:57:42 +0300

    row

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import imgSer1 from "./img/Icon.svg"
 import imgSer2 from "./img/Icon1.svg"
 import imgSer4 from "./img/Icon (1).svg"
@@ -8,6 +8,8 @@ import imgSer from "./img/Icon (3).svg"
 import {BsArrowRightShort} from "react-icons/bs"
 
 const Service = () => {
+    const [set, setSet] = useState(false)
+
     return (
         <section>
             <div className="container">
@@ -18,55 +20,74 @@ const Service = () => {
                         work to understand your issues <br/>
                         and are driven to ask better questions in the pursuit of making work.</p>
 
-                    <div className="flex grid grid-cols-3 mt-6">
+                    <div className="mt-6">
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16 rounded-full" src={imgSer} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Order Management</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
+                      <div className="flex">
+                          <div
+                              className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                              <img className="w-16 rounded-full" src={imgSer} alt=""/>
+                              <h2 className="text-white text-2xl leading-loose">Order Management</h2>
+                              <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                  elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                              <button onClick={() => setSet(!set)} className="text-white mt-7 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2 flex items-center">Learn
+                                  more<BsArrowRightShort/></button>
+                          </div>
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16" src={imgSer1} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Social Assistant</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
+                          <div
+                              className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                              <img className="w-16" src={imgSer1} alt=""/>
+                              <h2 className="text-white text-2xl leading-loose">Social Assistant</h2>
+                              <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                  elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                              <button onClick={() => setSet(!set)} className="text-white mt-7 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2 flex items-center">Learn
+                                  more<BsArrowRightShort/></button>
+                          </div>
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16" src={imgSer2} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Crypto Platform</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
+                          <div
+                              className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                              <img className="w-16" src={imgSer2} alt=""/>
+                              <h2 className="text-white text-2xl leading-loose">Crypto Platform</h2>
+                              <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                  elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                              <button onClick={() => setSet(!set)} className="text-white mt-7 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2 flex items-center">Learn
+                                  more<BsArrowRightShort/></button>
+                          </div>
+                      </div>
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16 rounded-full" src={imgSer3} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Smart Trading Modules</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16 " src={imgSer4} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Analyzer of the News</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
 
-                        <div className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
-                            <img className="w-16" src={imgSer5} alt=""/>
-                            <h2 className="text-white text-2xl leading-loose">Module of Price Notification</h2>
-                            <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
-                                elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
-                            <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
-                        </div>
+
+                        { set ?
+                            <div className="flex">
+                                <div
+                                    className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                                    <img className="w-16 rounded-full" src={imgSer3} alt=""/>
+                                    <h2 className="text-white text-2xl leading-loose">Smart Trading Modules</h2>
+                                    <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                        elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                                    <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
+                                </div>
+
+                                <div
+                                    className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                                    <img className="w-16 " src={imgSer4} alt=""/>
+                                    <h2 className="text-white text-2xl leading-loose">Analyzer of the News</h2>
+                                    <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                        elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                                    <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
+                                </div>
+                                <div
+                                    className="flex flex-col text-center items-center bg-gray-800 bg-opacity-30 p-9 rounded-3xl m-4 border border-gray-700">
+                                    <img className="w-16" src={imgSer5} alt=""/>
+                                    <h2 className="text-white text-2xl leading-loose">Module of Price Notification</h2>
+                                    <p className="text-white opacity-70">Lorem ipsum dolor sit amet, consectetur the adipiscing
+                                        elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                                    <a className="flex items-center text-white pt-7" href="#">Learn more<BsArrowRightShort/></a>
+                                </div>
+                            </div>
+                            : false
+
+                        }
 
                     </div>
                 </div>
